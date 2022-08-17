1a89896b9d2eac7294d5eaf571148f06449817f6
Berkay Sahin <berkaysahindev@gmail.com>
2022-08-17 20:02:31 +0300

    Early char controller
    new input system imported

---
+++ Snappy Gurus Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+
+public class PlayerController : MonoBehaviour
+{
+   private Rigidbody _rigidbody;
+   private InputManager _InputManager;
+}
+
+public class PlayerAnimation
+{
+   
+}
