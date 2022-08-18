e51408433306a8a56f9e22f3f118b6b562986ee2
Berkay Sahin <berkaysahindev@gmail.com>
2022-08-19 00:35:41 +0300

    Add Office Model

--- Snappy Gurus Jam/Assets/Scripts/Pickable.cs
+++ Snappy Gurus Jam/Assets/Scripts/Pickable.cs
@@ -1,7 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickable : MonoBehaviour
 {
+    public float AllowDistance;
+    public bool IsBeingHold { get; set; }
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var velocity = _rigidbody.velocity;
+        _rigidbody.velocity = IsBeingHold ? new Vector3(velocity.x,0.1f,velocity.z) : _rigidbody.velocity;
+    }
 }
