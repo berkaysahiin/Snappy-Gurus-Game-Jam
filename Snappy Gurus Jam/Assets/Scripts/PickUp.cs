38be3883eefd0869ff190b088412d2882e68290a
Berkay Sahin <berkaysahindev@gmail.com>
2022-08-18 14:41:28 +0300

    update

--- Snappy Gurus Jam/Assets/Scripts/PickUp.cs
+++ Snappy Gurus Jam/Assets/Scripts/PickUp.cs
@@ -1,17 +1,65 @@
 ﻿using System;
+using Interfaces;
+using JetBrains.Annotations;
 using SB;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PickUp : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private Transform holdPosition;
+    [SerializeField] private Transform orientation;
+
+    private bool isCurrentlyHolding;
+    [CanBeNull] private GameObject selectableObject => GetSelectableObject();
+    
     private RaycastHit hit;
+    
     private void Update()
     {
-        var ray = mainCamera.ScreenPointToRay(InputManager.MousePosition);
+        if (InputManager.InteractButton)
+        {
+            CarryObject();
+        }
+        else
+        {
+            ReleaseObject();
+        }
         
-        if (!Physics.Raycast(ray, out hit)) return;
-        var selection = hit.transform;
-        Debug.Log(selection);
+        print(selectableObject);
+    }
+
+
+    [CanBeNull]
+    private GameObject GetSelectableObject()
+    {
+        if (isCurrentlyHolding == false)
+        {
+            var ray = mainCamera.ScreenPointToRay(InputManager.MousePosition);
+            Physics.Raycast(ray,out hit);
+        }
+
+        if (hit.collider != null && hit.transform.gameObject.GetComponent<Pickable>() != null)
+        {
+            return hit.transform.gameObject;
+        }
+        
+        return null;
+    }
+    
+    private void CarryObject()
+    {
+        isCurrentlyHolding = true;
+        if (selectableObject != null)
+        {
+            selectableObject.transform.position = holdPosition.position;
+            selectableObject.transform.rotation = orientation.rotation;
+        }
+            
+    }
+    private void ReleaseObject()
+    {
+        isCurrentlyHolding = false;
     }
 }
